499387db809d13e92f5368ed4907074ce102b49c
Miguel Molina <hi@mvader.me>
2016-12-10 19:02:03 +0100

    Exercise 1 of day 6

---
+++ 2016/day6/signals.clj
@@ -0,0 +1,13 @@
+(use 'clojure.java.io)
+(use 'clojure.string)
+
+(defn most-frequent [coll]
+  (first (first (sort-by val > (frequencies coll)))))
+
+(defn -main []
+  (with-open [rdr (reader "./data.txt")]
+    (let [lines (map trim (line-seq rdr))]
+      (println (apply str 
+                      (map-indexed (fn [idx _]
+                                     (most-frequent (map #(first (drop idx %)) lines))) 
+                                   (first lines)))))))
