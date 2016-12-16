b4d9eff3d4c26f81550f2f76a76ab103be30d672
Miguel Molina <hi@mvader.me>
2016-12-16 18:49:46 +0100

    Add exercises for day 8

---
+++ 2016/day8/auth.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Pixel bool
+
+const (
+	On  Pixel = true
+	Off Pixel = false
+)
+
+type Screen [6][50]Pixel
+
+var (
+	rectRegex   = regexp.MustCompile(`^rect (\d+)x(\d+)`)
+	rotateRegex = regexp.MustCompile(`^rotate (column|row) (x|y)=(\d+) by (\d+)`)
+)
+
+func (s *Screen) Execute(ins string) {
+	if strings.HasPrefix(ins, "rect") {
+		parts := rectRegex.FindStringSubmatch(ins)
+		s.Rect(toInt(parts[1]), toInt(parts[2]))
+	} else if strings.HasPrefix(ins, "rotate") {
+		parts := rotateRegex.FindStringSubmatch(ins)
+		if parts[1] == "column" {
+			s.RotateColumn(toInt(parts[3]), toInt(parts[4]))
+		} else if parts[1] == "row" {
+			s.RotateRow(toInt(parts[3]), toInt(parts[4]))
+		}
+	}
+}
+
+func (s *Screen) Set(i, j int, state Pixel) {
+	(*s)[i][j] = state
+}
+
+func (s *Screen) Rect(a, b int) {
+	for i := 0; i < b; i++ {
+		for j := 0; j < a; j++ {
+			s.Set(i, j, On)
+		}
+	}
+}
+
+func (s *Screen) RotateColumn(col, n int) {
+	var tmp [6]Pixel
+	for i, r := range *s {
+		tmp[(i+n)%6] = r[col]
+	}
+
+	for i, state := range tmp {
+		s.Set(i, col, state)
+	}
+}
+
+func (s *Screen) RotateRow(row, n int) {
+	var tmp [50]Pixel
+	for i, p := range (*s)[row] {
+		tmp[(i+n)%50] = p
+	}
+
+	for i, state := range tmp {
+		s.Set(row, i, state)
+	}
+}
+
+func (s Screen) NumPixelsLit() (pixels int) {
+	for _, r := range s {
+		for _, c := range r {
+			if c == On {
+				pixels++
+			}
+		}
+	}
+	return
+}
+
+func toInt(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
+
+func main() {
+	data, err := ioutil.ReadFile("./data.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	instructions := strings.Split(string(data), "\n")
+	screen := Screen{}
+	for _, i := range instructions {
+		screen.Execute(i)
+	}
+
+	fmt.Println(screen.NumPixelsLit())
+}
