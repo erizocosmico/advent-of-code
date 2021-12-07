f2f43802c35fef903e753203269878412e162a1a
Miguel Molina <miguel@erizocosmi.co>
2021-12-07 10:23:20 +0100

    day 7 of 2021
    Signed-off-by: Miguel Molina <miguel@erizocosmi.co>

---
+++ 2021/07/07.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+let positions = try String(contentsOfFile: "./input.txt", encoding: .utf8)
+	.split(separator: ",")
+	.map { Int($0)! }
+
+var (min, max) = (positions.min()!, positions.max()!)
+let mid = (max + min) / 2
+let inLastHalf = positions.filter { $0 > mid }.count
+if inLastHalf >= positions.count / 2 {
+	min = mid
+} else {
+	max = mid
+}
+
+func triangularFuelCost(_ steps: Int) -> Int {
+	return (steps * steps + steps) / 2
+}
+
+func constantFuelCost(_ steps: Int) -> Int { return steps }
+
+func fuelNeededToAlign(_ positions: [Int], _ position: Int, _ fuelCost: (Int) -> Int) -> Int {
+	return positions.map { p in fuelCost(abs(p - position)) }.reduce(0, +)
+}
+
+print("*", (min...max).map { fuelNeededToAlign(positions, $0, constantFuelCost) }.min()!)
+print("**", (min...max).map { fuelNeededToAlign(positions, $0, triangularFuelCost) }.min()!)
